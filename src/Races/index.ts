a1ff15104332718b851f0166cae183d9485cce20
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-09-24 09:24:48 -0300

    Feat: implementado requisito 2

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -4,5 +4,6 @@ import Orc from './Orc';
 import Elf from './Elf';
 import Halfling from './Halfling';
 
-export { Dwarf, Orc, Halfling, Elf }; 
+export { Dwarf, Orc, Halfling, Elf };
+
 export default Race;
